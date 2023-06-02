1bb2423da5c547a7ceab77c8e3741ce86a451cff
mfelczynski <mifelmi@gmail.com>
2023-06-02 22:04:54 +0200

    qq

--- jsScrapper.js
+++ jsScrapper.js
@@ -5,18 +5,21 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('https://www.otomoto.pl/osobowe/nowe?search%5Border%5D=created_at_first%3Adesc');
 
-    const cars = await page.$$eval('\\#\\36 110947528 > div.ooa-1cq16zj.evg565y15 > h2 > a', links =>
+
+    const cars = await page.$$eval('div.ooa-1cq16zj.evg565y15 > h2 > a', links =>
         links.map(link => ({
             name: link.textContent.trim(),
             url: link.href
         }))
     );
-
-    const prices = await page.$$eval('\\#\\36 110947528 > div:nth-child(3) > div > span', prices =>
+    // #\36 110957361 > div:nth-child(5) > div > span
+    // #\36 110959181 > div:nth-child(3) > div > span
+    // #\33 2aef38d-0065-11ee-b75a-f654cad4d416 > div.ooa-14ryoaz.e189ctmp4 > div.evg565y10.ooa-dsk6y6.er34gjf0 > span
+    const prices = await page.$$eval('div:nth-child(3) > div > span', prices =>
         prices.map(price => parseFloat(price.textContent.replace(/\s/g, '')))
     );
 
-    const volumes = await page.$$eval('\\#\\36 110947353 > div.ooa-1cq16zj.evg565y15 > div > ul > li:nth-child(3)', volumes =>
+    const volumes = await page.$$eval('div.ooa-1cq16zj.evg565y15 > div > ul > li:nth-child(3)', volumes =>
         volumes.map(volume => parseInt(volume.textContent.replace(/\s/g, '')))
     );
 
